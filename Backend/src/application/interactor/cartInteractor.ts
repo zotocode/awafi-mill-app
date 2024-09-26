69cfabbc886bea9d18495d40a4c78937d904a32c
Athul pk <athulprathap96@gmail.com>
2024-09-26 13:07:40 +0530

    main prage erros handled - ready to merge

--- Backend/src/application/interactor/cartInteractor.ts
+++ Backend/src/application/interactor/cartInteractor.ts
@@ -1,4 +1,4 @@
-import { Cart } from "../../domin/entities/userCart";
+import { Cart } from "../../domain/entities/userCart";
 import { IUserRepo } from "../../interface/userInterface/IuserRepo";
 import { ICartInteractor } from "../../interface/cartInterface/IcartInteractor";
 import { ICartRepo } from "../../interface/cartInterface/IcartRepo";
