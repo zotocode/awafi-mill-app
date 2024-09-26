93993241980f61b1618c63f46c7fe325b039157c
abhishek <abhiabhishektr@gmail.com>
2024-09-26 12:59:09 +0530

    Remove Backend/node_modules and Backend/dist from tracking

--- Backend/dist/application/services/bcrypt.d.ts
+++
@@ -1,6 +0,0 @@
-import { IBcrypt } from "../../interface/serviceInterface/bcryptInterface";
-export declare class HashPassword implements IBcrypt {
-    constructor();
-    encryptPassword(password: string): Promise<string>;
-    comparePassword(password: string, hashedPassword: string): Promise<boolean>;
-}
--- Backend/dist/interface/userInterface/IuserInteractor.d.ts
+++
@@ -1,5 +0,0 @@
-export interface IUserInteractor {
-    login(email: string, password: string): Promise<any>;
-    registerUser(data: any): Promise<any>;
-    verifyOtp(data: any): Promise<any>;
-}
