02851db28e79683c43eda9d3d8d52fa623aac49e
Athul pk <athulprathap96@gmail.com>
2024-09-26 13:01:39 +0530

    merge conflict handleed

--- Backend/src/interface/userInterface/IuserRepo.ts
+++ Backend/src/interface/userInterface/IuserRepo.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import { User } from "../../domain/userSchema"
 import { IuserDocument } from "../../infrastrucutre/model/userModel"
-=======
-import { User } from "../../domin/userSchema"
-import { IuserDocument } from "../../infrastruture/model/userModel"
 import { InewUserData } from "../../types/userTypes/userInteractorTypes"
 
->>>>>>> upstream/main
 
 export interface IUserRepo{
  findUser(email:string):Promise<any>
