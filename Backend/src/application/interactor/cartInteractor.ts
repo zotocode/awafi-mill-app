05463d865bdaf67435701c98dd622d67fe27bb8c
Athul pk <athulprathap96@gmail.com>
2024-09-26 12:39:08 +0530

    feat: user cart  intialized

---
+++ Backend/src/application/interactor/cartInteractor.ts
@@ -0,0 +1,22 @@
+import { Cart } from "../../domin/entities/userCart";
+import { IUserRepo } from "../../interface/userInterface/IuserRepo";
+import { ICartInteractor } from "../../interface/cartInterface/IcartInteractor";
+import { ICartRepo } from "../../interface/cartInterface/IcartRepo";
+import { CartRepo } from "../../infrastruture/repositories/cartRepo";
+
+
+
+export class CartInteractor implements ICartInteractor {
+    private cartRepo : ICartRepo;
+
+    constructor(cartRepo:ICartRepo){
+        this.cartRepo = cartRepo
+    }
+
+async cartIteams(): Promise<any> {
+    console.log('====================================');
+    console.log("reached cart interactor");
+    console.log('====================================');
+}
+
+}
