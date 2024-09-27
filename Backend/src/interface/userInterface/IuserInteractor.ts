811218e6448ebd632c9f09fea630473922e42b41
Abhishek T R <130368708+abhiabhishektr@users.noreply.github.com>
2024-09-27 16:48:23 +0530

    Revert "user registation updated"

--- Backend/src/interface/userInterface/IuserInteractor.ts
+++ Backend/src/interface/userInterface/IuserInteractor.ts
@@ -4,9 +4,8 @@ import { UserInteractorResp } from "../../types/userTypes/userInteractorTypes"
 
 export interface IUserInteractor{
     login(email:string,password:string):Promise<UserInteractorResp>
-    registerUser(email:string,name:string,password:string,phone:number):Promise<any>
-    verifyOtp(email: string, otp: string):Promise<any>
-    editProfile(data:any):Promise<any>
+    registerUser(email:string,password:string):Promise<any>
+    verifyOtp(data:any):Promise<any>
 
 }
 
