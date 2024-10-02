141aa7e6c8bef1a7e7a00d07b342c3d7ccf5ffe9
shahabas ap <shahabasap5@gmail.com>
2024-10-02 13:26:23 +0530

    admin login page  and dashboard initiated

--- Frontend-admin/src/App.tsx
+++ Frontend-admin/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import LoginPage from './pages/Login';
+import DashboardPage from './pages/Dashboard';
 
 function App() {
   return (
-    <Routes>
-       <Route path="/" element={<Login />} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
+    </Router>
   );
 }
 
