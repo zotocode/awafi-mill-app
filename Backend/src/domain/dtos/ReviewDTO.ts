692924371cf0d3eea1d13045e5691a63ea59fba4
abhishek <abhiabhishektr@gmail.com>
2024-10-16 18:50:11 +0530

    review rating completed

---
+++ Backend/src/domain/dtos/ReviewDTO.ts
@@ -0,0 +1,14 @@
+export interface ReviewDTO {
+    userId: string;
+    productId: string;
+    rating: number;
+    comment?: string;
+  }
+  
+  export interface CreateReviewDTO {
+    userId: string;
+    productId: string;
+    rating: number;
+    comment?: string;
+  }
+  
