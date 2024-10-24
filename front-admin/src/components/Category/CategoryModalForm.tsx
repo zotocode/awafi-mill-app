8c18ca2fd4e58855128f716cfaf860433b1395ed
shahabas ap <shahabasap5@gmail.com>
2024-10-24 14:36:04 +0530

    product ,category, subcategory first stage completed and new template added

--- front-admin/src/components/Category/CategoryModalForm.tsx
+++ front-admin/src/components/Category/CategoryModalForm.tsx
@@ -196,7 +196,7 @@ const CategoryModalForm: React.FC<CategoryModalFormProps> = ({
             <button
               type="submit"
               disabled={isSubmitting}
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-50"
+              className="w-full text-white bg-black  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-50"
             >
               {isSubmitting ? (category ? 'Updating...' : 'Adding...') : category ? 'Update Category' : 'Add Category'}
             </button>
