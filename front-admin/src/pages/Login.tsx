029cb8db8e5e9be69b4d5ba2d91e4ecfe95a200a
Athul pk <athulprathap96@gmail.com>
2024-10-24 17:53:43 +0530

    all merge conflict cleared

--- front-admin/src/pages/Login.tsx
+++ front-admin/src/pages/Login.tsx
@@ -1,11 +1,9 @@
 // src/pages/Login.jsx
-<<<<<<< HEAD
-import LoginContainer from '../components/LoginContainer';
-=======
+
+
 import React from 'react';
 import Navbar from '../components/Header';
 import LoginContainer from '../components/Login/LoginContainer';
->>>>>>> upstream/develop
 
 const LoginPage = () => {
     const handleLogin = (email: string, password: string) => {
