9b8851a6ef78484270bc1b46e25d77a6ca779cbf
abhishek <abhiabhishektr@gmail.com>
2024-10-26 23:56:02 +0530

    wishlist changes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
-import 
+import
 userRoute from "./presentation/routes/userRoute";
 import productRoute from "./presentation/routes/productRoute";
 import cartRoutes from "./presentation/routes/cartRoute";
@@ -10,6 +10,7 @@ import cors from "cors";
 import morgan from "morgan";
 import adminRoute from "./presentation/routes/adminRoute";
 import logger from "./utilities/logger";
+import wishlistRoutes from "./presentation/routes/wishlistRoute";
 
 
 const startServer = async (): Promise<void> => {
@@ -33,15 +34,16 @@ const startServer = async (): Promise<void> => {
 
 
     app.use('/api/user', userRoute);
-    app.use('/api/admin',adminRoute)
+    app.use('/api/admin', adminRoute)
     app.use('/api/products', productRoute);
-    app.use('api/cart',cartRoutes)
+    app.use('api/cart', cartRoutes)
+    app.use('api/wishlist', wishlistRoutes)
     app.use('/api/categories', categoryRoute);
     app.use('/api/banner', bannerRoutes);
 
 
 
-    
+
 
     // 500 - Internal Server Error handler
     app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
